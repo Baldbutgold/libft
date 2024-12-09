39c569875bae754df31a491d3fe3bde18695fbfd
brr <abdouplayz2003@gmail.com>
2024-12-09 05:21:56 +0100

    cleaned a bunch of just chatgpt useless tests

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -18,32 +18,3 @@ int	ft_isalpha(int c)
 		return (1);
 	return (0);
 }
-
-/*#include <stdio.h>*/
-/*#include <ctype.h>*/
-/**/
-/*int ft_isalpha(int c);*/
-/**/
-/*int main() {*/
-/*    int test_cases[] = { */
-/*        'A', 'Z', 'a', 'z', '0', '9', '!', '@', */
-/*        '[', '`', ' ', '\t', '\n', EOF */
-/*    };*/
-/*    int num_tests = sizeof(test_cases) / sizeof(test_cases[0]);*/
-/**/
-/*    printf("Testing ft_isalpha:\n");*/
-/*    printf("%-10s %-10s %-10s\n", */
-/*           "Input", "isalpha", "ft_isalpha");*/
-/*    printf("------------------------------------\n");*/
-/**/
-/*    for (int i = 0; i < num_tests; i++) {*/
-/*        int c = test_cases[i], */
-/*            expected = isalpha(c) != 0, */
-/*            actual = ft_isalpha(c);*/
-/*        printf("%-10c %-10d %-10d\n", */
-/*               (c >= 32 && c <= 126) ? c : '?', */
-/*               expected, actual);*/
-/*    }*/
-/**/
-/*    return 0;*/
-/*}*/
